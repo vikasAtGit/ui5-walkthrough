cd86602f829b9075eb868e66bf44f8bdef4bf58d
vikasAtGit <vk.mishravikas@gmail.com>
2021-12-24 15:47:57 +0530

    XMLView

--- webapp/index.js
+++ webapp/index.js
@@ -1,9 +1,12 @@
 sap.ui.define([
-    "sap/m/Text"
-], function(Text) {
+    "sap/ui/core/mvc/XMLView"
+
+], function(XMLView) {
     "use strict";
 
-    new Text({
-        text: "Hi UI5!"
-    }).placeAt("content")
+    XMLView.create({
+        viewName: "sap.ui.demo.walkthrough.view.App"
+    }).then(function(oView) {
+        oView.placeAt("content");
+    });
 });
