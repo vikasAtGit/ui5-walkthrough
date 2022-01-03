e313a7594e549e5d821e6ed0894d4eac4d4f188a
vikasAtGit <vk.mishravikas@gmail.com>
2022-01-03 14:53:55 +0530

    i18n ResourceBundle

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,9 +1,10 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
 
-], function(Controller, MessageToast, JSONModel) {
+], function(Controller, MessageToast, JSONModel, ResoureModel) {
     "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough", {
@@ -16,11 +17,23 @@ sap.ui.define([
             }
             var oModel = new JSONModel(oData);
             this.getView().setModel(oModel);
+
+            // setting resource model 
+            var i18nModel = new ResoureModel({
+                bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
+                supportedLocales: [''],
+                fallbackLocale: ""
+            });
+            this.getView().setModel(i18nModel, "i18n");
         },
 
         onShowHello: function() {
+            // read msg from i18n model 
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
+            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            var sMsg = oBundle.getText("helloMsg", [sRecipient]);
             // show native or vanilla js alert
-            MessageToast.show("Hi there from controller!");
+            MessageToast.show(sMsg);
         }
     })
 })
