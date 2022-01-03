f1e5e241e9925fe918bba89c8886f165c9beb45b
vikasAtGit <vk.mishravikas@gmail.com>
2022-01-03 13:05:49 +0530

    JSONModel

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,23 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
 
-], function(Controller, MessageToast) {
+], function(Controller, MessageToast, JSONModel) {
     "use strict";
+
     return Controller.extend("sap.ui.demo.walkthrough", {
+
+        onInit: function() {
+            var oData = {
+                recipient: {
+                    name: "Recipient Name"
+                }
+            }
+            var oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
+        },
+
         onShowHello: function() {
             // show native or vanilla js alert
             MessageToast.show("Hi there from controller!");
