5777c49b8c7fe3927f4c892c723cd0017e620c49
vikasAtGit <vk.mishravikas@gmail.com>
2022-01-11 12:21:12 +0530

    Nested Views

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,21 +1,8 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
+    "sap/ui/core/mvc/Controller"
 
-], function(Controller, MessageToast, JSONModel, ResoureModel) {
+], function(Controller) {
     "use strict";
 
-    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-
-        onShowHello: function() {
-            // read msg from i18n model 
-            var oBundle = this.getView().getModel("i18n").getResourceBundle();
-            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-            var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-            // show native or vanilla js alert
-            MessageToast.show(sMsg);
-        }
-    })
+    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {})
 })
